621df932c899885013c19fa1b41ece054d2b3496
Brandi <pvtpyro@yahoo.com>
2025-08-12 10:35:17 -0700

    add dummy cart data

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { Item } from "./item/item";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
-  imports: [Item],
+  imports: [Item, CommonModule],
   templateUrl: './cart.html',
   styleUrl: './cart.scss'
 })
 export class Cart {
 
+    lemonades = [
+        {lemonJuice: 10, sugar: 0, iceCubes: 7, price: 10.99},
+        {lemonJuice: 10, sugar: 2, iceCubes: 7, price: 4.99},
+        {lemonJuice: 10, sugar: 8, iceCubes: 1, price: 2.00},
+        {lemonJuice: 10, sugar: 8, iceCubes: 7, price: 6.99},
+        {lemonJuice: 2, sugar: 1, iceCubes: 2, price: 2.33333},
+        {lemonJuice: 10, sugar: 8, iceCubes: 7, price: 2.02},
+    ]
 }
--- src/app/cart/item/item.ts
+++ src/app/cart/item/item.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Glass } from "../../lemonade/glass/glass";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-item',
-  imports: [Glass],
+  imports: [Glass, CommonModule],
   templateUrl: './item.html',
   styleUrl: './item.scss'
 })
 export class Item {
 
+    @Input() lemonade: any;
+    
 }
