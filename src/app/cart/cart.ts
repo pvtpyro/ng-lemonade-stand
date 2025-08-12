3962f22062a289168a81bbad269b9b7d6a9a4234
Brandi <pvtpyro@yahoo.com>
2025-08-12 16:07:09 -0700

    populate lemonade stand on cart. in case we want the user to change stands?

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Item } from "./item/item";
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 interface ILemonade {
@@ -11,15 +12,35 @@ interface ILemonade {
     price: number
 }
 
+interface LemonadeStand {
+    id: number,
+    name: string
+}
+
 
 @Component({
     selector: 'app-cart',
-    imports: [Item, CommonModule],
+    imports: [Item, CommonModule, ReactiveFormsModule],
     templateUrl: './cart.html',
     styleUrl: './cart.scss'
 })
 export class Cart {
 
+    lemonadeStands: LemonadeStand[] = [
+        { id: 1, name: 'Cooksys Lemonade Stand 1' },
+        { id: 2, name: 'Cooksys Lemonade Stand 2' },
+        { id: 3, name: 'Cooksys Lemonade Stand 3' },
+        { id: 4, name: 'Cooksys Lemonade Stand 4' },
+        { id: 5, name: 'Cooksys Lemonade Stand 5' }
+    ];
+
+    customerForm: FormGroup = new FormGroup({
+        selectedStand: new FormControl<LemonadeStand | undefined>(undefined, [Validators.required])
+    })
+
+    onSubmit() {
+        console.log(`Selected Lemonade Stand: ${JSON.stringify(this.customerForm.controls['selectedStand'].value)}`)
+    }
 
     @Input() lemonades: ILemonade[] = [];
 
@@ -27,7 +48,7 @@ export class Cart {
 
     totalPrice: number = 0;
 
-    receiveLemonadeId(id:number) {
+    receiveLemonadeId(id: number) {
         this.secondPassLemonadeIdEvent.emit(id);
     }
 
@@ -37,6 +58,10 @@ export class Cart {
         this.lemonades.forEach(
             (lemonade) => (this.totalPrice = this.totalPrice + lemonade.price)
         );
+
+        this.customerForm.setValue({
+            selectedStand:  this.lemonadeStands[0]
+        })
     }
 
 }
