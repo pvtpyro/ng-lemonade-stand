1b4610045c2eecaac0fc1e06406ae2d15db44ff3
Brandi <pvtpyro@yahoo.com>
2025-08-12 16:42:46 -0700

    setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CustomerForm } from './customer-form/customer-form';
+import { Lemonade } from './lemonade/lemonade';
+import { Checkout } from './checkout/checkout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: CustomerForm
+    },
+    {
+        path: 'lemonade',
+        component: Lemonade
+    },
+    {
+        path: 'checkout',
+        component: Checkout
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Lemonade } from "./lemonade/lemonade";
 import { CustomerForm } from "./customer-form/customer-form";
+import { Checkout } from "./checkout/checkout";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, Lemonade, CustomerForm],
+    imports: [CommonModule, Lemonade, CustomerForm, Checkout, RouterOutlet],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
