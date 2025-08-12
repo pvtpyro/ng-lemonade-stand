3ed830c57e6ab8200a1c048bc928de8f11de238f
Brandi <pvtpyro@yahoo.com>
2025-08-12 11:16:33 -0700

    read data for cart

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,21 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Item } from "./item/item";
 import { CommonModule } from '@angular/common';
 
+
+interface ILemonade {
+    lemonJuice: number,
+    sugar: number,
+    iceCubes: number,
+    price: number
+}
+
+
 @Component({
-  selector: 'app-cart',
-  imports: [Item, CommonModule],
-  templateUrl: './cart.html',
-  styleUrl: './cart.scss'
+    selector: 'app-cart',
+    imports: [Item, CommonModule],
+    templateUrl: './cart.html',
+    styleUrl: './cart.scss'
 })
 export class Cart {
 
-    lemonades = [
-        {lemonJuice: 10, sugar: 0, iceCubes: 7, price: 10.99},
-        {lemonJuice: 10, sugar: 2, iceCubes: 7, price: 4.99},
-        {lemonJuice: 10, sugar: 8, iceCubes: 1, price: 2.00},
-        {lemonJuice: 10, sugar: 8, iceCubes: 7, price: 6.99},
-        {lemonJuice: 2, sugar: 1, iceCubes: 2, price: 2.33333},
-        {lemonJuice: 10, sugar: 8, iceCubes: 7, price: 2.02},
-    ]
+
+    @Input() lemonades: ILemonade[] = [];
+
+    totalPrice: number = 0;
+
+    ngOnInit(): void {
+        this.lemonades.forEach(
+            (lemonade) => (this.totalPrice = this.totalPrice + lemonade.price)
+        );
+    }
+
 }
--- src/app/lemonade/lemonade.ts
+++ src/app/lemonade/lemonade.ts
@@ -14,6 +14,13 @@ interface IProduct {
     unit: string
 }
 
+interface ILemonade {
+    lemonJuice: number,
+    sugar: number,
+    iceCubes: number,
+    price: number
+}
+
 @Component({
     selector: 'app-lemonade',
     imports: [Card, Product, CommonModule, Glass, Cart, CartIcon],
@@ -46,7 +53,12 @@ export class Lemonade {
             max: 12,
             unit: "cubes"
         }
-    ]
+    ];
+
+
+    cartLemonades: ILemonade[] = [];
+
+    currentLemonade: ILemonade = { lemonJuice: 0, sugar: 0, iceCubes: 0, price: 0 };
 
     increment(productName: string) {
         Lemonade.bind(this)
@@ -69,6 +81,57 @@ export class Lemonade {
 
     toggleCart() {
         this.cartVisible = !this.cartVisible;
+    }
+
+    addToCart() {
+        if(this.products[0].amount > 0 || this.products[1].amount > 0 || this.products[2].amount > 0) {
+            this.currentLemonade = {
+                lemonJuice: this.products[0].amount,
+                sugar: this.products[1].amount,
+                iceCubes: this.products[2].amount,
+                price: 0
+            };
+            this.currentLemonade.price = this.calculatePrice();
+            this.cartLemonades.push(this.currentLemonade);
+            console.log(this.cartLemonades);
+            this.resetProducts();
+        }
+    }
+
+    calculatePrice(): number {
+        return (
+            this.currentLemonade.lemonJuice * this.products[0].price +
+            this.currentLemonade.sugar * this.products[1].price +
+            this.currentLemonade.iceCubes * this.products[2].price
+        );
+    }
+
+
+    resetProducts() {
+        // this is wet. I dont like it
+        this.products = [
+            {
+                name: 'Lemon Juice',
+                price: 0.5,
+                amount: 0,
+                max: 10,
+                unit: 'oz',
+            },
+            {
+                name: 'Sugar',
+                price: 0.25,
+                amount: 0,
+                max: 8,
+                unit: 'tsp',
+            },
+            {
+                name: 'Ice',
+                price: 0.05,
+                amount: 0,
+                max: 12,
+                unit: 'cubes',
+            },
+        ];
     }
 
 }
